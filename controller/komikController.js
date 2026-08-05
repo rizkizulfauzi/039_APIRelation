7bc35ac3f648e35c1bcf7568c1b480b396d032d6
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2026-08-05 14:45:47 +0700

    add getAll function to komikController for retrieving all komiks with related authors and genres

--- controller/komikController.js
+++ controller/komikController.js
@@ -3,3 +3,28 @@ const db = require("../models");
 const Komik = db.Komik;
 const Penulis = db.Penulis;
 const Genre = db.Genre;
+
+async function getAll(req, res) {
+  try {
+    const komiks = await Komik.findAll({
+      include: [
+        {
+          model: Penulis,
+          as: "penulis",
+          attributes: ["id", "nama", "email"]
+        },
+        {
+          model: Genre,
+          as: "genres",
+          attributes: ["id", "nama"]
+        }
+      ]
+    });
+
+    return res.status(200).json(komiks);
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message
+    });
+  }
+}
